184449a5f45a7b26cfb357366a0378b624d7850a
ekhorash <ekhorash@cisco.com>
2016-06-08 20:44:40 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-//	"io"
 	"fmt"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-//	io.WriteString(w, "Hi there, You are the BEST")
 }
 
 func main() {
